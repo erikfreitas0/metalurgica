9192a0329a8258bae7dbec279593afe5abce86d0
Erik de Freitas <133702197+erikfreitas0@users.noreply.github.com>
2024-09-03 14:30:16 -0300

    criando os arquivos do back-end

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from database.db import db
+from flask_cors import CORS
+from routes.index import default_routes
+
+class App:
+    def __init__(self):
+        self.app = Flask(__name__)
+        CORS(self.app)
+        self.app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:''@localhost/metalurgica' 
+        db.init_app(self.app)
+        default_routes(self.app) 
+
+    def run(self):
+        return self.app.run(port=3000, host='localhost', debug=True)
+
+app = App()
+app.run()
---
+++ models/usuario.py
@@ -0,0 +1,21 @@
+from database.db import db
+
+class usuario(db.Model):
+    def to_dict(self):
+        return{
+            'codigo': self.codigo,
+            'nome': self.nome,
+            'cpf': self.cpf,
+            'email': self.email,
+            'telefone': self.telefone,
+            'senha': self.senha,
+        }
+    codigo = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100))
+
+    def __init__(self, nome):
+        self.nome = nome
+        self.cpf = cpf
+        self.email = email
+        self.telefone = telefone
+        self.senha = senha
