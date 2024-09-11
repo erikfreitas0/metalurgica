eca01c7fddc0fab68f2d1c34744d8e7827064186
Erik de Freitas <133702197+erikfreitas0@users.noreply.github.com>
2024-09-11 14:59:55 -0300

    arrumando erros do pip e codigo

--- models/produto.py
+++ models/produto.py
@@ -1,8 +1,25 @@
 from database.db import db
 
-class produto(db.Model):
+class Produto(db.Model):
+    # Definição das colunas
+    codigo = db.Column(db.Integer, primary_key=True)
+    tipo = db.Column(db.String(100))
+    peso = db.Column(db.Float)
+    espessura = db.Column(db.String(100))
+    durabilidade = db.Column(db.Integer)
+    local = db.Column(db.String(100))
+    preco = db.Column(db.Float)
+
+    def __init__(self, tipo, peso, espessura, durabilidade, local, preco):
+        self.tipo = tipo
+        self.peso = peso
+        self.espessura = espessura
+        self.durabilidade = durabilidade
+        self.local = local
+        self.preco = preco 
+
     def to_dict(self):
-        return{
+        return {
             'codigo': self.codigo,
             'tipo': self.tipo,
             'peso': self.peso,
@@ -11,19 +28,3 @@ class produto(db.Model):
             'local': self.local,
             'preco': self.preco
         }
-    codigo = db.Column(db.Integer, primary_key=True)
-    tipo = db.Column(db.String(100))
-    peso = db.Column(db.Float(100))
-    espessura = db.Column(db.String(100))
-    durabilidade = db.Column(db.Integer(20))
-    local = db.Column(db.String(100))
-    preco = db.Column(db.Float(100))
-    
-
-    def __init__(self, tipo):
-        self.tipo = tipo
-        self.peso = peso
-        self.espessura = espessura
-        self.durabilidade = durabilidade
-        self.local = local
-        self.preco = preco
--- models/usuario.py
+++ models/usuario.py
@@ -1,8 +1,25 @@
 from database.db import db
 
-class usuario(db.Model):
+class Usuario(db.Model):
+    # Definição das colunas
+    codigo = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100))
+    cpf = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    telefone = db.Column(db.String(20))  # Alterado para String para acomodar código de área e números
+    senha = db.Column(db.String(100))
+    login = db.Column(db.String(100))
+
+    def __init__(self, nome, cpf, email, telefone, senha, login):
+        self.nome = nome
+        self.cpf = cpf
+        self.email = email
+        self.telefone = telefone
+        self.senha = senha
+        self.login = login 
+
     def to_dict(self):
-        return{
+        return {
             'codigo': self.codigo,
             'nome': self.nome,
             'cpf': self.cpf,
@@ -11,18 +28,3 @@ class usuario(db.Model):
             'senha': self.senha,
             'login': self.login
         }
-    codigo = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(100))
-    cpf = db.Column(db.String(100))
-    email = db.Column(db.String(100))
-    telefone = db.Column(db.Integer(20))
-    senha = db.Column(db.String(100))
-    login = db.Column(db.String(100))
-
-    def __init__(self, nome):
-        self.nome = nome
-        self.cpf = cpf
-        self.email = email
-        self.telefone = telefone
-        self.senha = senha
-        self.login = login
