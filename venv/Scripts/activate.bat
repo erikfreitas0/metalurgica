76ff51506c00ab3ef865349b8c3530cc68ddf757
Erik de Freitas <133702197+erikfreitas0@users.noreply.github.com>
2024-09-17 21:37:32 -0300

    arrumando venv dnv

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\erik.194918\Desktop\metalurgica\back-end\venv
+set VIRTUAL_ENV=C:\Users\erikd\OneDrive\Área de Trabalho\tcc\back-end\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
