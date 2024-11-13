59e49b5257716f9c626aeec3a9cc338433d994a5
Erik de Freitas <133702197+erikfreitas0@users.noreply.github.com>
2024-11-13 14:17:43 -0300

    mudança na main pra enviar pro banco via html

--- controllers/produtoController.py
+++ controllers/produtoController.py
@@ -1,4 +1,4 @@
-from flask import request, render_template
+from flask import Blueprint, request, jsonify
 from database.db import db
 from models.produto import Produto
 
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 from database.db import db
 from flask_cors import CORS
 from routes.index import default_routes
@@ -6,10 +6,20 @@ from routes.index import default_routes
 class App:
     def __init__(self):
         self.app = Flask(__name__)
-        CORS(self.app)
+        CORS(self.app, resources={r"/*": {"origins": "*"}})
         self.app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:''@localhost/metalurgica' 
         db.init_app(self.app)
-        default_routes(self.app) 
+        default_routes(self.app)
+
+        # Função para lidar com requisições OPTIONS
+        @self.app.before_request
+        def handle_options_requests():
+            if request.method == 'OPTIONS':
+                response = self.app.make_response('')
+                response.headers['Access-Control-Allow-Origin'] = '*'
+                response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+                response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+                return response
 
     def run(self):
         return self.app.run(port=3000, host='localhost', debug=True)
