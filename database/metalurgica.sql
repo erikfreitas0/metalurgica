e790bdeda355725b68777d31995d69c8ecc3e886
Erik de Freitas <133702197+erikfreitas0@users.noreply.github.com>
2024-12-10 13:47:27 -0300

    banco

--- database/metalurgica.sql
+++ database/metalurgica.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tempo de Geração: 24/09/2024 às 19h27min
+-- Tempo de Geração: 10/12/2024 às 17h46min
 -- Versão do Servidor: 5.5.20
 -- Versão do PHP: 5.3.9
 
@@ -23,6 +23,21 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `mensagem`
+--
+
+CREATE TABLE IF NOT EXISTS `mensagem` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `telefone` varchar(20) NOT NULL,
+  `mensagem` varchar(500) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `produto`
 --
 
@@ -30,10 +45,20 @@ CREATE TABLE IF NOT EXISTS `produto` (
   `codigo` int(100) NOT NULL AUTO_INCREMENT,
   `tipo` varchar(50) NOT NULL,
   `peso` float(10,2) NOT NULL,
-  `espessura` varchar(50) NOT NULL,
+  `espessura` float(10,2) NOT NULL,
   `preco` float(10,2) NOT NULL,
   PRIMARY KEY (`codigo`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+
+--
+-- Extraindo dados da tabela `produto`
+--
+
+INSERT INTO `produto` (`codigo`, `tipo`, `peso`, `espessura`, `preco`) VALUES
+(9, 'ferro', 50.00, 5.00, 25.00),
+(10, 'ferro', 62.00, 8.00, 32.00),
+(11, 'aço inoxidável', 40.00, 3.00, 41.00),
+(12, 'alumínio', 90.00, 60.00, 35.00);
 
 -- --------------------------------------------------------
 
@@ -47,19 +72,20 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   `cpf` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
   `telefone` varchar(20) NOT NULL,
-  `senha` varchar(50) NOT NULL,
   `login` varchar(50) NOT NULL,
+  `senha` varchar(50) NOT NULL,
   PRIMARY KEY (`codigo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 --
 -- Extraindo dados da tabela `usuario`
 --
 
-INSERT INTO `usuario` (`codigo`, `nome`, `cpf`, `email`, `telefone`, `senha`, `login`) VALUES
-(1, 'aaa', '123', 'aaaa@gmail.com', '999122349', 'user', 'senha'),
-(2, 'erik', '145', 'erik@gmail.com', '991225312', 'erik', '123'),
-(3, 'dudu', '444', 'pv@gmail.com', '991225312', 'pv', '123');
+INSERT INTO `usuario` (`codigo`, `nome`, `cpf`, `email`, `telefone`, `login`, `senha`) VALUES
+(4, 'erik de freitas', '145.670.799-06', 'erikdefreitas5@gmail.com', '(48) 99122-5312', '123', 'erik'),
+(5, 'gilson de freitas', '645.638.639-04', 'gilferacos@hotmail.com', '(48) 99984-5183', 'dido6919', 'dido'),
+(6, 'pedro venicio', '123.456.789-10', 'pvviadao@gmail.com', '(48) 58899-4521', '123', 'pvgay'),
+(7, 'eduardo samuel', '123.456.789-11', 'duducorinthians@gmail.com', '(48) 58899-4785', '123', 'dudu');
 
 -- --------------------------------------------------------
 
@@ -72,22 +98,9 @@ CREATE TABLE IF NOT EXISTS `vendedor` (
   `nome` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `cpf` varchar(50) NOT NULL,
+  `telefone` varchar(20) NOT NULL,
   `login` varchar(50) NOT NULL,
-  `senha` varchar(20) NOT NULL,
-  `telefone` varchar(20) NOT NULL,
-  PRIMARY KEY (`codigo`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-CREATE TABLE IF NOT EXISTS `mensagem` (
-  `codigo` int(5) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `telefone` varchar(20) NOT NULL,
-  `mensagem` varchar(200) NOT NULL,
+  `senha` varchar(50) NOT NULL,
   PRIMARY KEY (`codigo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
